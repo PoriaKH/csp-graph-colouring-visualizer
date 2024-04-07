0f01b2c47345036f38769d8321af3ca27ec8418f
PoriaKH <poria.gm@gmail.com>
2024-04-08 00:55:14 +0330

    removing unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/App.css';
-import './style/w3.css';
-import './style/w3-round.css';
 import './style/form.css';
 import { Copied } from "./components/graph4";
 
